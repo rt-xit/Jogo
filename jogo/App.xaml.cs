0bad12cd2ddb755e75eda842054a0c226e535bfb
Renato <renato.domingues@escola.pr.gov.br>
2024-04-04 09:45:30 -0300

    projeto limpo

---
+++ jogo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace jogo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ jogo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace jogo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
